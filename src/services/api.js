c30b966a406087f879fa758399f7115a613863ae
LeskarX <leskarzone123@gmail.com>
2026-03-15 02:48:31 +0530

    added api

---
+++ src/services/api.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:5000/api';
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('staynest_token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response?.status === 401) {
+      localStorage.removeItem('staynest_token');
+      localStorage.removeItem('staynest_user');
+    }
+    return Promise.reject(err);
+  }
+);
+
+export default api;
